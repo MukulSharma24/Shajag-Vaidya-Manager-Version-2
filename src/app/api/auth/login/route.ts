a00cea8a44c8cacf32b6e9c0895741c73f56b44e
Shajagsys <arun.iitk24@gmail.com>
2026-02-17 12:59:11 +0530

    Update route.ts

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -1,4 +1,5 @@
 // src/app/api/auth/login/route.ts
+export const dynamic = "force-dynamic";
 import { NextRequest, NextResponse } from 'next/server';
 import { prisma } from '@/lib/prisma';
 import { verifyPassword, signJWT } from '@/lib/auth';
